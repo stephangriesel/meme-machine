17da510ac7ba346726bbbcd1804b1c347ee334b4
Steve <sgriesel@gmail.com>
2019-05-03 14:37:33 +0200

    state & video working - remove background image

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/Video.css';
+import snowflakes from '../img/snowflakes.gif'
 
 
 class Video extends Component {
@@ -8,31 +9,35 @@ class Video extends Component {
 
         this.textInput = React.createRef();
         this.state = {
-            value: 'https://upload.wikimedia.org/wikipedia/en/transcoded/6/61/Old_Man_Drinking_a_Glass_of_Beer_%281897%29.webm/Old_Man_Drinking_a_Glass_of_Beer_%281897%29.webm.360p.webm'
+            value: ''
         }
     }
 
-        handleSubmit = e => {
-            e.preventDefault();
-            this.setState({ value: this.textInput.current.value })
-        };
-        render() {
-            console.log(this.props);
-            return (
-                <React.Fragment>
-                    <div className="add-video">
-                        <h3>Add Your Own</h3>
-                        <form onSubmit={this.handleSubmit}>
-                            <input
-                                type='text'
-                                ref={this.textInput}>
-                            </input>
-                            <button type="submit" value="submit">Go!</button>
-                        </form>
-                    </div>
-                    <p>Your video: {this.state.value}</p>
-                    <div className="video-wrapper">
+    handleSubmit = e => {
+        e.preventDefault();
+        this.setState({ value: this.textInput.current.value })
+    };
 
+    render() {
+        console.log(this.state)
+        return (
+            <React.Fragment>
+                <div className="center">
+                    <p>Here are some demo videos:</p>
+                    <p><a href="http://dl3.webmfiles.org/big-buck-bunny_trailer.webm">Demo #1</a></p>
+                    <p><a href="http://dl3.webmfiles.org/elephants-dream.webm">Demo #2</a></p>
+                    <p>Your video: {this.state.value}</p>
+                </div>
+                <div className="add-video">
+                    <h3>Add Your Own</h3>
+                    <form onSubmit={(e) => { e.preventDefault(); this.setState({ showVideo: true }) }}>
+                        <input type="text" value={this.state.value} onChange={(e) => this.setState({ value: e.target.value })} />
+                        <button type="submit" value="submit">Go!</button>
+                    </form>
+
+                </div>
+                <div className="video-wrapper">
+                    {this.state.showVideo ?
                         <video
                             maxwidth="800"
                             width="100%"
@@ -47,11 +52,15 @@ class Video extends Component {
                                 type="video/webm"
                             />
                         </video>
-
-                    </div>
-                </React.Fragment>
-            )
-        }
+                        :
+                        <div >
+                        <img className="vidPlaceholder" src={snowflakes} />;
+                        </div>
+                        }
+                </div>
+            </React.Fragment >
+        )
     }
+}
 
-    export default Video;
+export default Video;
